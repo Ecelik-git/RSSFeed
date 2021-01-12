4e3f67211c2b739130c0e8afe96e19ecb0dcd23e
Elvan Celik <63591827+Ecelik-git@users.noreply.github.com>
2021-01-12 09:08:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ NewsFeed = feedparser.parse(input("Enter RSS: "))
 
 print('Number of RSS posts :', len(NewsFeed.entries))
 
-entry = NewsFeed.entries[1]
-
-#printing title, description and link
-print('Post Title :',entry.title)
-print('Post Description :',entry.description)
-print('Post link :',entry.link)
+for i in range(len(NewsFeed.entries)):
+  entry = NewsFeed.entries[i]
+  #printing title, description and link
+  print('Post Title :',entry.title)
+  print('Post Description :',entry.description)
+  print('Post link :',entry.link)
